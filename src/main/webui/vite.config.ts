b3c0b325e22f91150d6e3fd7bc58dc6abd2fc05f
Daniel Alonso <daniel.alonso@magnolia-cms.com>
2024-01-24 12:52:52 +0100

    Introduced quinoa for ruling ui building process|live-development|ui-routing and other stuff

--- src/main/webui/vite.config.ts
+++ src/main/webui/vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: '../src/main/resources/META-INF/resources'
+    rollupOptions: {
+      input: {
+        quinoa: resolve(__dirname, 'index.html'),
+      },
+    },
   },
 })
