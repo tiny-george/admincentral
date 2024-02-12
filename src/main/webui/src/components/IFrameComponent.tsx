cc58f1788542f4660723c254dc62fd2f3e5f2a64
Jorge Franco <jorge.franco@magnolia-cms.com>
2024-02-12 14:48:48 +0100

    Use same events as magnolia

--- src/main/webui/src/components/IFrameComponent.tsx
+++ src/main/webui/src/components/IFrameComponent.tsx
@@ -1,13 +1,29 @@
-import {IFrameState} from "./IFrameState.tsx";
+import { IFrameState } from "./IFrameState.tsx";
+import {RefObject, useEffect, useRef} from "react";
+
+const initIframe = (ref: RefObject<HTMLIFrameElement>, id: string, value: string) => {
+    setTimeout(() => {
+        ref.current?.contentWindow?.postMessage({
+            action: "init",
+            correlationId: id,
+            state: { value: value }
+        }, '*');
+    }, 500);
+}
 
 // @ts-ignore
 export const IFrameComponent = ({baseUrl, fieldName, fieldValue}) => {
-  const fieldId = fieldName + Date.now();
+  const fieldId = Math.floor(Math.random() * 10000) + "";
+  const iframeRef = useRef<HTMLIFrameElement>(null);
+
+    useEffect(() => {
+        initIframe(iframeRef, fieldId, fieldValue);
+        return () => {};
+    }, [fieldValue]);
 
   return (
       <>
-        <iframe id={fieldId} className="w-100 h-100"
-                src={`${baseUrl}?id=${encodeURIComponent(fieldId)}&value=${encodeURIComponent(fieldValue)}`}></iframe>
+        <iframe ref={iframeRef} id={fieldId} className="w-100 h-100" src={baseUrl}></iframe>
         <IFrameState fieldId={fieldId} fieldValue={fieldValue} fieldName={fieldName}/>
       </>
   )
--- src/main/webui/src/components/IFrameState.tsx
+++ src/main/webui/src/components/IFrameState.tsx
@@ -3,7 +3,9 @@ import { useState } from "react";
 const listeners: any = {};
 const registerComponentChange = (id: string, currentValueFunction: () => string, listenerFunction: (v: string) => void) => {
     const eventFunction = (event: MessageEvent<any>) => {
-        if (event.data.type === 'COMPONENT_STATE_CHANGE' && event.data.id === id) {
+        if (event.data.action === 'changeValue' && event.data.correlationId === id) {
+            //console.log("Received event...");
+            //console.log(event.data);
             const newValue = event.data.value;
             if (currentValueFunction() !== newValue) {
                 listenerFunction(newValue);
@@ -20,17 +22,17 @@ const registerComponentChange = (id: string, currentValueFunction: () => string,
 // @ts-ignore
 export const IFrameState = ({fieldId, fieldName, fieldValue}) => {
 
-  const [componentValue, setComponentValue] = useState(fieldValue)
+    const [componentValue, setComponentValue] = useState(fieldValue)
 
-  registerComponentChange(fieldId, () => componentValue , (newValue: string) => {
-      setComponentValue(newValue);
-      //console.log("CHANGE VALUE TO: " + newValue);
-  });
+    registerComponentChange(fieldId, () => componentValue , (newValue: string) => {
+        setComponentValue(newValue);
+        //console.log("CHANGE VALUE TO: " + newValue);
+    });
 
-  return (
+    return (
       <>
         <input name={fieldName} type={'hidden'} value={componentValue}/>
         <p>Value: {componentValue}</p>
       </>
-  )
+    )
 }
